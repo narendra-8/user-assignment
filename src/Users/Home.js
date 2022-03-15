caf68bfc8e8026a0cee654463871cc70485ee6f8
narendra-8 <86469490+narendra-8@users.noreply.github.com>
2022-03-15 15:49:36 +0530

    Update Home.js

--- src/Users/Home.js
+++ src/Users/Home.js
@@ -1,6 +1,6 @@
 
 function Home(){
-    return (<h2 className="heading-style">WELCOME TO THE USERS HOME PAGE</h2>)
+    return (<h2 className="heading-style">WELCOME TO THE USERS PAGE HRMS</h2>)
 }
 
 export {Home};
